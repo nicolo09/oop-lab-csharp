e7b1e9297c77f6f0785ac880fe35e99189d95e65
Nicolò Guerra <guerragianni0503@gmail.com>
2021-12-14 11:19:39 +0100

    Implemented Complex

--- 4-ExtensionMethods/Complex.cs
+++ 4-ExtensionMethods/Complex.cs
@@ -20,66 +20,82 @@ namespace ExtensionMethods
         }
 
         /// <inheritdoc cref="IComplex.Real"/>
-        public double Real
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Real { get; private set; }
 
         /// <inheritdoc cref="IComplex.Imaginary"/>
-        public double Imaginary
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Imaginary { get; private set; }
 
         /// <inheritdoc cref="IComplex.Modulus"/>
-        public double Modulus
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Modulus { get => Math.Sqrt(Math.Pow(Real, 2) + Math.Pow(Imaginary, 2)); }
 
         /// <inheritdoc cref="IComplex.Phase"/>
-        public double Phase
-        {
-            get
-            {
-                throw new System.NotImplementedException();
-            }
-        }
+        public double Phase { get => Math.Atan2(Imaginary, Real); }
 
         /// <inheritdoc cref="IComplex.ToString"/>
         public override string ToString()
         {
-            // TODO improve
-            return base.ToString();
+            string n = "";
+            if (Real > 0)
+            {
+                n = "+" + Real.ToString();
+            }
+            else if (Real < 0)
+            {
+                n = Real.ToString();
+            }
+            else
+            {
+                //Real=0
+            }
+
+
+            if (Imaginary * Imaginary == 1)
+            {
+                n = n + (Imaginary == 1 ? "+i" : "-i");
+            }
+            else if (Imaginary > 0)
+            {
+                n = n + "+" + Imaginary.ToString();
+            }
+            else if (Imaginary < 0)
+            {
+                n = n + Imaginary.ToString();
+            }
+            else
+            {
+                //Immaginary=0
+            }
+
+            if (Real == 0 && Imaginary == 0) return "0";
+
+            return n;
         }
 
         /// <inheritdoc cref="IEquatable{T}.Equals(T)"/>
         public bool Equals(IComplex other)
         {
-            throw new System.NotImplementedException();
+            return other != null &&
+                   Real == other.Real &&
+                   Imaginary == other.Imaginary;
         }
 
         /// <inheritdoc cref="object.Equals(object?)"/>
         public override bool Equals(object obj)
         {
-            // TODO improve
-            return base.Equals(obj);
+            if (obj is Complex)
+            {
+                return Equals((Complex)obj);
+            }
+            else
+            {
+                return false;
+            }
         }
 
         /// <inheritdoc cref="object.GetHashCode"/>
         public override int GetHashCode()
         {
-            // TODO improve
-            return base.GetHashCode();
+            return HashCode.Combine(Real, Imaginary);
         }
     }
 }
